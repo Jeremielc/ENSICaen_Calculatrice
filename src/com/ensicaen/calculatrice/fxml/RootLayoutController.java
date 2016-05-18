fea919398d428a603fcdb2ddc64e5b42160bb3eb
Pierrick Hue <lesurferdusud@hotmail.fr>
2016-05-18 16:13:37 +0200

    Initial commit

---
+++ src/com/ensicaen/calculatrice/fxml/RootLayoutController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ensicaen.calculatrice.fxml;
+
+/**
+ *
+ * @author lesurfer
+ */
+public class RootLayoutController {
+    public void Addittion(){
+        
+    }
+    public void Substract(){
+        
+    }
+    public void Multiply(){
+        
+    }
+    public void Divide(){
+        
+    }
+}
---
+++ src/com/ensicaen/calculatrice/main/MainApp.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ensicaen.calculatrice.main;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author lesurfer
+ */
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            BorderPane rootLayout;
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/com/ensicaen/calculatrice/fxml/RootLayout.fxml"));
+            rootLayout = loader.load();
+
+            Scene scene = new Scene(rootLayout);
+
+            primaryStage.setResizable(false);
+            primaryStage.setTitle("Calculatrice");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
